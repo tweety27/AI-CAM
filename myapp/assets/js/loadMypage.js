183d6325d959fc25fce26add91e8a0f8a7282843
tweety27 <135826584+tweety27@users.noreply.github.com>
2025-01-17 10:45:23 +0900

    Delete myapp directory

--- myapp/assets/js/loadMypage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const manageProjectsContainer = document.querySelector(".manage-projects-container");
-
-    manageProjectsContainer.addEventListener("click", function () {
-        const targetPage = "../templates/mypage.html";
-        window.location.href = targetPage;
-    });
-});
